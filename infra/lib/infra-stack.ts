1cff3e79a4199f3c931a095831de7f42a042922d
Matthew Enarle <enarlem10@gmail.com>
2025-11-20 20:56:33 +0800

    feat: wire cdk http api health endpoint

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { HttpApi } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
+import { RustFunction } from 'cargo-lambda-cdk';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // rust lambda built from lambda/api
+    const apiLambda = new RustFunction(this, 'api_lambda', {
+      manifestPath: '../lambda/api',
+      runtime: 'provided.al2023',
+      timeout: cdk.Duration.seconds(10),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const httpApi = new HttpApi(this, 'http_api');
+    const healthIntegration = new HttpLambdaIntegration('health_integration', apiLambda);
+
+    httpApi.addRoutes({
+      path: '/health',
+      methods: [HttpMethod.GET],
+      integration: healthIntegration,
+    });
+
+    new cdk.CfnOutput(this, 'api_url', {
+      value: httpApi.apiEndpoint,
+    });
   }
 }
